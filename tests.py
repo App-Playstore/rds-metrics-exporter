beb11b1807b1fdae0bf77a1183b2e7f68791e867
Ogechuku Chibogu <ochibogu@liveperson.com>
2024-12-02 22:31:41 +0100

    feat: update unit test

--- tests.py
+++ tests.py
@@ -14,6 +14,7 @@ class TestExporter(unittest.TestCase):
                 {
                     'DBInstanceIdentifier': 'my-db-instance',
                     'EngineVersion': '16.3'
+                    'Engine': 'postgres'
                 }
             ]
         }
